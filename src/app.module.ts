9a2d54c53a1364112675cc03b4402210dc4a11e0
yohannes <yohannesg218@gmail.com>
2026-04-18 11:17:42 -0700

    email

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { EmailModule } from './email/email.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, EmailModule],
   controllers: [AppController],
   providers: [AppService],
 })

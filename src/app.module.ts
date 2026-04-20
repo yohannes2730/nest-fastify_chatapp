574f48733edc714ab0cbf1b9407abe12deefbec6
yohannes <yohannesg218@gmail.com>
2026-04-20 11:35:02 -0700

    add  auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EmailModule } from './email/email.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [UsersModule, EmailModule],
+  imports: [UsersModule, EmailModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
